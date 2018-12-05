a655b5135a66bb34a2f283e637c793c926c43a66
CJackYang <cjackyang@qq.com>
2018-12-05 15:26:10 +0800

    add BFile

---
+++ src/fruitmix/backup/file.js
@@ -0,0 +1,16 @@
+const File = require('../vfs/file')
+const debug = require('debug')('ws:backup-file')
+
+class BFile extends File {
+  /**
+    @rewrite 
+  */
+ abspath() { 
+  if (!this.ctx) throw new Error('node.abspath: node is already destroyed')
+  let q = [] 
+  for (let n = this.parent; n !== null; n = n.parent) q.unshift(n)
+    return path.join(this.ctx.dir, ...q.map(n => n.name), this.hash)
+  }
+}
+
+module.exports = BFile
